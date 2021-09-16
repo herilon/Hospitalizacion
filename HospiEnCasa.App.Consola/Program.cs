934e4e0c57efe5f107b35e24ac6e75ce8aae5324
Heri <hlondono@misena.edu.com>
2021-09-16 18:47:33 -0500

    First commit

---
+++ HospiEnCasa.App.Consola/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using HospiEnCasa.App.Dominio;
+using HospiEnCasa.App.Persistencia;
+
+namespace HospiEnCasa.App.Consola
+{
+    class Program
+    {
+        private static IRepositorioPaciente _repoPaciente = new RepositorioPaciente(new Persistencia.AppContext());
+        private static IRepositorioMedico _repoMedico = new RepositorioMedico(new Persistencia.AppContext());
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //AddPaciente();
+            //BuscarPaciente(1);
+            //EliminarPaciente(2);
+            //MostrarPacientes();
+            //AddMedico();
+            AsignarMedico();
+        }
+        private static void AddPaciente()
+        {
+            var paciente = new Paciente
+            {
+                Nombre = "Juanito",
+                Apellidos = "Perez",
+                NumeroTelefono = "3001645",
+                Genero = Genero.Masculino,
+                Direccion = "Calle 4 No 7-4",
+                Longitud = 5.07062F,
+                Latitud = -75.52290F,
+                Ciudad = "Manizales",
+                FechaNacimiento = new DateTime(1990, 04, 12)
+            };
+            _repoPaciente.AddPaciente(paciente);
+        }
+        private static void BuscarPaciente(int idPaciente)
+        {
+            var paciente = _repoPaciente.GetPaciente(idPaciente);
+            Console.WriteLine(paciente.Nombre + " " + paciente.Apellidos);
+        }
+        private static void EliminarPaciente(int idPaciente)
+        {
+            _repoPaciente.DeletePaciente(idPaciente);
+            Console.WriteLine("paciente Eliminado");
+        }
+        private static void MostrarPacientes()
+        {
+            IEnumerable<Paciente> pacientes = _repoPaciente.GetAllPacientes();
+            foreach (var paciente in pacientes)
+            {
+                Console.WriteLine(paciente.Nombre + " " + paciente.Apellidos);
+            }
+        }
+        private static void AddMedico()
+        {
+            var medico = new Medico
+            {
+                Nombre = "Juliana",
+                Apellidos = "Lopez",
+                NumeroTelefono = "3001645",
+                Genero = Genero.Femenino,
+                Especialidad = "Internista",
+                Codigo = "2535",
+                RegistroRethus = "34567"
+            };
+            _repoMedico.AddMedico(medico);
+        }
+
+        private static void AsignarMedico()
+        {
+            var medico = _repoPaciente.AsignarMedico(1, 5);
+            Console.WriteLine(medico.Nombre + " " + medico.Apellidos);
+        }
+    }
+}
---
+++ HospiEnCasa.App.Persistencia/AppRepositorios/RepositorioPaciente.cs
@@ -0,0 +1,76 @@
+using System.Collections.Generic;
+using HospiEnCasa.App.Dominio;
+
+namespace HospiEnCasa.App.Persistencia
+{
+    public class RepositorioPaciente : IRepositorioPaciente
+    {
+        private readonly AppContext _appContext;
+
+        public RepositorioPaciente(AppContext appContext)
+        {
+            _appContext = appContext;
+        }
+
+        Paciente IRepositorioPaciente.AddPaciente(Paciente paciente)
+        {
+            var pacienteAdicionado = _appContext.Pacientes.Add(paciente);
+            _appContext.SaveChanges();
+            return pacienteAdicionado.Entity;
+        }
+
+        void IRepositorioPaciente.DeletePaciente(int idPaciente)
+        {
+            var pacienteEncontrado = _appContext.Pacientes.Find(idPaciente);
+            if (pacienteEncontrado == null)
+                return;
+            _appContext.Pacientes.Remove(pacienteEncontrado);
+            _appContext.SaveChanges();
+        }
+
+        IEnumerable<Paciente> IRepositorioPaciente.GetAllPacientes()
+        {
+            return _appContext.Pacientes;
+        }
+
+        Paciente IRepositorioPaciente.GetPaciente(int idPaciente)
+        {
+            return _appContext.Pacientes.Find(idPaciente);
+        }
+
+        Paciente IRepositorioPaciente.UpdatePaciente(Paciente paciente)
+        {
+            var pacienteEncontrado = _appContext.Pacientes.Find(paciente.Id);
+            if (pacienteEncontrado != null)
+            {
+                pacienteEncontrado.Nombre = paciente.Nombre;
+                pacienteEncontrado.Apellidos = paciente.Apellidos;
+                pacienteEncontrado.NumeroTelefono = paciente.NumeroTelefono;
+                pacienteEncontrado.Genero = paciente.Genero;
+                pacienteEncontrado.Direccion = paciente.Direccion;
+                pacienteEncontrado.Latitud = paciente.Latitud;
+                pacienteEncontrado.Longitud = paciente.Longitud;
+                pacienteEncontrado.Ciudad = paciente.Ciudad;
+                pacienteEncontrado.FechaNacimiento = paciente.FechaNacimiento;
+                _appContext.SaveChanges();
+            }
+            return pacienteEncontrado;
+        }
+        Medico IRepositorioPaciente.AsignarMedico(int idPaciente, int idMedico)
+        {
+            var pacienteEncontrado = _appContext.Pacientes.Find(idPaciente);
+            if (pacienteEncontrado != null)
+            {
+                var medicoEncontrado = _appContext.Medicos.Find(idMedico);
+                if (medicoEncontrado != null)
+                {
+                    pacienteEncontrado.Medico = medicoEncontrado;
+                    _appContext.SaveChanges();
+                }
+                return medicoEncontrado;
+            }
+            return null;
+        }
+    }
+}
+
